70db2c28d8726caefdf7d471ce8ccfc2c9424271
eduard_knats <cdecdckev@gmail.com>
2026-08-14 00:56:51 +0700

    add example immutable class

---
+++ ImmutableExample/Immutable.java
@@ -0,0 +1,34 @@
+package Immutable;
+
+
+public final class Immutable {
+
+    private final Mutable value;
+
+    public Immutable (Mutable value) {
+        this.value = new Mutable(value.getName());
+    }
+
+    public Mutable getValue() {
+        return new Mutable(value.getName());
+    }
+
+
+    public static void main(String[] args) {
+        Mutable mutable = new Mutable("имя до изменения");
+        Immutable immutable = new Immutable(mutable);
+        mutable.setName("имя после изменения");
+
+        System.out.println("Попытка изменения через конструктор");
+        System.out.println("immutable " + immutable.value.getName());
+        System.out.println("mutable " + mutable.getName());
+        System.out.println();
+
+        System.out.println("Попытка изменения через getter");
+        Mutable mutableCopy = immutable.getValue();
+        mutableCopy.setName("новое имя");
+        System.out.println("immutable " + immutable.value.getName());
+        System.out.println("mutable " + mutableCopy.getName());
+    }
+}
+
